6fb60bb90aef5275e40349fad233c86826298b81
blcklamb <whenucan35@gmail.com>
2023-05-09 16:19:01 +0900

    add: chapter 02_03_첫 puppeteer 크롤링

--- lecture/index_02.js
+++ lecture/index_02.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import parse from "csv-parse/lib/sync.js";
 import fs from "fs";
 import puppeteer from "puppeteer";
@@ -6,35 +7,31 @@ const csv = fs.readFileSync("csv/data.csv");
 const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
-  const browser = await puppeteer.launch({
-    headless: process.env.NODE_ENV === "production",
-  });
-  const [page01, page02, page03] = await Promise.all([
-    browser.newPage(),
-    browser.newPage(),
-    browser.newPage(),
-  ]);
-  await Promise.all([
-    page01.goto("https://velog.io/@blcklamb"),
-    page02.goto("https://naver.com"),
-    page03.goto("https://google.com"),
-  ]);
+  try {
+    const browser = await puppeteer.launch({
+      headless: process.env.NODE_ENV === "production",
+    });
+    await Promise.all(
+      records.map(async (r, i) => {
+        try {
+          const page = await browser.newPage();
+          await page.goto(r[1]);
+          const scoreEl = await page.$(".info");
+          if (scoreEl) {
+            const text = await page.evaluate((tag) => tag.textContent, scoreEl);
+            console.log(r[0], "평점", text.split("평점 ")[1].split(" ")[0]);
+          }
+          await page.close();
+        } catch (e) {
+          console.error(e);
+        }
+      })
+    );
 
-  // 안되는 친구
-  //   await page.waitForFunction(() => {
-  //     setTimeout(() => console.log("delayed for 3 seconds"), 3000);
-  //   });
-
-  await Promise.all([
-    page01.waitForTimeout(3000),
-    page02.waitForTimeout(1000),
-    page03.waitForTimeout(2000),
-  ]);
-  await page01.close();
-  await page02.close();
-  await page03.close();
-
-  await browser.close();
+    await browser.close();
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 crawler();
