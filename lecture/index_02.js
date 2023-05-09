144e626c9d8ef13f9e0aec000aa55d635b619370
blcklamb <whenucan35@gmail.com>
2023-05-09 17:18:56 +0900

    add: chapter 02_04_csv에 출력하기

--- lecture/index_02.js
+++ lecture/index_02.js
@@ -2,12 +2,15 @@ import { log } from "console";
 import parse from "csv-parse/lib/sync.js";
 import fs from "fs";
 import puppeteer from "puppeteer";
+import stringify from "csv-stringify/lib/sync.js";
+// const stringify = require("csv-stringify/lib/sync");
 
 const csv = fs.readFileSync("csv/data.csv");
 const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
   try {
+    const result = [];
     const browser = await puppeteer.launch({
       headless: process.env.NODE_ENV === "production",
     });
@@ -16,10 +19,13 @@ const crawler = async () => {
         try {
           const page = await browser.newPage();
           await page.goto(r[1]);
-          const scoreEl = await page.$(".info");
+          const scoreEl = await page.$(
+            "._au_movie_content_wrap .detail_info .info"
+          );
           if (scoreEl) {
             const text = await page.evaluate((tag) => tag.textContent, scoreEl);
-            console.log(r[0], "평점", text.split("평점 ")[1].split(" ")[0]);
+            result[i] = [r[0], r[1], text.split("평점 ")[1].split(" ")[0]];
+            // console.log(r[0], "평점", text.split("평점 ")[1].split(" ")[0]);
           }
           await page.close();
         } catch (e) {
@@ -27,8 +33,9 @@ const crawler = async () => {
         }
       })
     );
-
     await browser.close();
+    const str = stringify(result);
+    fs.writeFileSync("csv/result_chap02.csv", str);
   } catch (e) {
     console.error(e);
   }
