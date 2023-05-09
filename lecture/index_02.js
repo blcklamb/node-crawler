2d43259f9e444940acd0f07f7d0354b48b8cbe86
blcklamb <whenucan35@gmail.com>
2023-05-09 17:47:13 +0900

    add: chapter 02_06_userAgent와 한 탭으로 크롤링 - csv 파일

--- lecture/index_02.js
+++ lecture/index_02.js
@@ -1,9 +1,7 @@
-import { log } from "console";
 import parse from "csv-parse/lib/sync.js";
 import fs from "fs";
 import puppeteer from "puppeteer";
 import stringify from "csv-stringify/lib/sync.js";
-// const stringify = require("csv-stringify/lib/sync");
 
 const csv = fs.readFileSync("csv/data.csv");
 const records = parse(csv.toString("utf-8"));
@@ -14,26 +12,26 @@ const crawler = async () => {
     const browser = await puppeteer.launch({
       headless: process.env.NODE_ENV === "production",
     });
-    await Promise.all(
-      records.map(async (r, i) => {
-        try {
-          const page = await browser.newPage();
-          await page.goto(r[1]);
-          const text = await page.evaluate(() => {
-            const score = document.querySelector(
-              "._au_movie_content_wrap .detail_info .info"
-            );
-            if (score) return score.textContent;
-          });
-          if (text) {
-            result[i] = [r[0], r[1], text.split("평점 ")[1].split(" ")[0]];
-          }
-          await page.close();
-        } catch (e) {
-          console.error(e);
-        }
-      })
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
     );
+    for (const [i, r] of records.entries()) {
+      await page.goto(r[1]);
+
+      console.log(await page.evaluate("navigator.userAgent"));
+      const text = await page.evaluate(() => {
+        const score = document.querySelector(
+          "._au_movie_content_wrap .detail_info .info"
+        );
+        if (score) return score.textContent;
+      });
+      if (text) {
+        result[i] = [r[0], r[1], text.split("평점 ")[1].split(" ")[0]];
+      }
+      await page.waitForTimeout(3000);
+    }
+    await page.close();
     await browser.close();
     const str = stringify(result);
     fs.writeFileSync("csv/result_chap02.csv", str);
